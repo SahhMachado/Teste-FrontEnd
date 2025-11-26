7cc50c2d65274abbdd22de5533d42c533f721afd
Sara Machado <sarasanto1309@gmail.com>
2025-11-25 21:29:42 -0300

    Feat: Create Header

---
+++ teste-frontend/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import LogoGit from "../assets/logoGitHub.svg"
+
+function Header() {
+    return(
+        <header className="w-full h-17 flex items-center bg-[#24292E] text-white">
+            <img src={LogoGit} className="ml-70 mr-10"></img>
+            <p className="mr-10">/</p>
+            <p className="font-roboto">Profile</p>
+        </header>
+    )
+}
+
+export default Header
