b0708f53345375ffd7155cb7fd4218b7f52b431b
Sara Machado <sarasanto1309@gmail.com>
2025-11-30 20:43:07 -0300

    Update: Adjusted header items

--- teste-frontend/src/components/Header.tsx
+++ teste-frontend/src/components/Header.tsx
@@ -3,8 +3,9 @@ import LogoGit from "../assets/logoGitHub.svg"
 function Header() {
     return(
         <header className="w-screen h-[8vh] flex items-center bg-[#24292E] text-white">
-            <img src={LogoGit} className="lg:ml-[15%] lg:w-[7vw] w-[16vw] mr-[2.5vw] ml-[3%]"></img>
-            <p className="mr-[2.5vw] text-2xl">/</p>
+            <img src={LogoGit} className="lg:ml-[15%] lg:w-[7vw] w-[16vw] 
+                                          lg:mr-[1.2%] md:mr-[2%] ml-[3%]"></img>
+            <p className="lg:mr-[1.2%] md:mr-[2%] text-2xl">/</p>
             <p className="font-light text-[1.7vh]">Profile</p>
         </header>
     )
